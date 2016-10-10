a5f5b9bcd28564801f0e2b34bc5efc59dfbee227
andaicheng <andaicheng@live.com>
2016-10-10 12:18:41 +0800

    JDK 8 time util

---
+++ src/main/java/com/andyadc/study/time/TimeUtil.java
@@ -0,0 +1,70 @@
+package com.andyadc.study.time;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+/**
+ * 基于 JDK 8 time包的时间工具类
+ *
+ * @author andaicheng
+ */
+public class TimeUtil {
+
+    /**
+     * 获取默认时间格式: yyyy-MM-dd HH:mm:ss
+     */
+    private static final DateTimeFormatter DEFAULT_DATETIME_FORMATTER = TimeFormat.LONG_DATE_PATTERN_LINE.formatter;
+
+    private TimeUtil() {
+    }
+
+    public static void main(String[] args) {
+        System.out.println(parseTime("2016-10-10 09:52:01"));
+    }
+
+    public static LocalDateTime parseTime(String timeStr) {
+        return LocalDateTime.parse(timeStr, DEFAULT_DATETIME_FORMATTER);
+    }
+
+    /**
+     * String 转时间
+     *
+     * @param timeStr
+     * @param format  时间格式
+     */
+    public static LocalDateTime parseTime(String timeStr, TimeFormat format) {
+        return LocalDateTime.parse(timeStr, format.formatter);
+    }
+
+    public enum TimeFormat {
+        /**
+         * 短时间格式
+         */
+        SHORT_DATE_PATTERN_LINE("yyyy-MM-dd"),
+        SHORT_DATE_PATTERN_SLASH("yyyy/MM/dd"),
+        SHORT_DATE_PATTERN_DOUBLE_SLASH("yyyy\\MM\\dd"),
+        SHORT_DATE_PATTERN_NONE("yyyyMMdd"),
+
+        /**
+         * 长时间格式
+         */
+        LONG_DATE_PATTERN_LINE("yyyy-MM-dd HH:mm:ss"),
+        LONG_DATE_PATTERN_SLASH("yyyy/MM/dd HH:mm:ss"),
+        LONG_DATE_PATTERN_DOUBLE_SLASH("yyyy\\MM\\dd HH:mm:ss"),
+        LONG_DATE_PATTERN_NONE("yyyyMMdd HH:mm:ss"),
+
+        /**
+         * 长时间格式 带毫秒
+         */
+        LONG_DATE_PATTERN_WITH_MILSEC_LINE("yyyy-MM-dd HH:mm:ss.SSS"),
+        LONG_DATE_PATTERN_WITH_MILSEC_SLASH("yyyy/MM/dd HH:mm:ss.SSS"),
+        LONG_DATE_PATTERN_WITH_MILSEC_DOUBLE_SLASH("yyyy\\MM\\dd HH:mm:ss.SSS"),
+        LONG_DATE_PATTERN_WITH_MILSEC_NONE("yyyyMMdd HH:mm:ss.SSS");
+
+        private transient DateTimeFormatter formatter;
+
+        TimeFormat(String pattern) {
+            formatter = DateTimeFormatter.ofPattern(pattern);
+        }
+    }
+}
