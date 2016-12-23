d722dce79cc557b20154f5de5bc95ceed038a64f
andyadc <andaicheng@gmail.com>
2016-12-23 21:29:45 +0800

    concurrency

---
+++ concurrency-study/src/main/java/com/andyadc/concurrency/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * @author andaicheng
+ * @version 2016/12/23
+ */
+package com.andyadc.concurrency;
---
+++ concurrency-study/src/main/java/com/andyadc/concurrency/threadpool/TestWithThreadPool.java
@@ -0,0 +1,38 @@
+package com.andyadc.concurrency.threadpool;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author andaicheng
+ * @version 2016/10/26
+ */
+public class TestWithThreadPool {
+
+    public static void main(String[] args) {
+        long start = System.currentTimeMillis();
+        final List<Integer> list = new LinkedList<>();
+        ThreadPoolExecutor executor = new ThreadPoolExecutor(100, 100, 60, TimeUnit.SECONDS, new LinkedBlockingQueue<>(20000));
+        final Random random = new Random();
+        for (int i = 0; i < 20000; i++) {
+            executor.execute(() -> list.add(random.nextInt()));
+        }
+
+        executor.shutdown();
+
+        try {
+            executor.awaitTermination(1, TimeUnit.DAYS);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println(System.currentTimeMillis() - start);
+        System.out.println(list.size());
+
+    }
+
+}
