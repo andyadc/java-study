a3044fbd146ffaea9ef3e7f7c6d50a75ec061e63
andaicheng <andaicheng@gmail.com>
2017-01-16 18:47:29 +0800

    Decorator structure

--- design-pattern/src/main/java/com/andyadc/designpattern/decorator/structure/ConcreteComponent.java
+++ design-pattern/src/main/java/com/andyadc/designpattern/decorator/structure/ConcreteComponent.java
@@ -1,6 +1,7 @@
 package com.andyadc.designpattern.decorator.structure;
 
 /**
+ * 具体构件(ConcreteComponent)角色：定义一个将要接收附加责任的类。
  * @author andaicheng
  * @version 2017/1/16
  */
--- design-pattern/src/main/java/com/andyadc/designpattern/decorator/structure/Decorator.java
+++ design-pattern/src/main/java/com/andyadc/designpattern/decorator/structure/Decorator.java
@@ -1,6 +1,7 @@
 package com.andyadc.designpattern.decorator.structure;
 
 /**
+ * 装饰(Decorator)角色：持有一个构件(Component)对象的实例，并定义一个与抽象构件接口一致的接口。
  * @author andaicheng
  * @version 2017/1/16
  */
