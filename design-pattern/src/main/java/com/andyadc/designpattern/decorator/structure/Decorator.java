a1dbd556021cf43f322cc6d15021e2eb0cf10dbe
andyadc <andaicheng@gmail.com>
2017-01-16 20:14:09 +0800

    Decorator structure

--- design-pattern/src/main/java/com/andyadc/designpattern/decorator/structure/Decorator.java
+++ design-pattern/src/main/java/com/andyadc/designpattern/decorator/structure/Decorator.java
@@ -11,6 +11,7 @@ public class Decorator implements Component {
     private Component component;
 
     public Decorator(Component component) {
+        this.component = component;
     }
 
     @Override
