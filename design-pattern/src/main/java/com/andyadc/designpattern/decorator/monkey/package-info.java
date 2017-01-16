5cdff7aaf61c4b601a5d2a65aedd85cde50c9f23
andyadc <andaicheng@gmail.com>
2017-01-16 21:17:56 +0800

    Decorator package-info

---
+++ design-pattern/src/main/java/com/andyadc/designpattern/decorator/monkey/package-info.java
@@ -0,0 +1,14 @@
+/**
+ * 孙悟空有七十二般变化，他的每一种变化都给他带来一种附加的本领。
+ * 他变成鱼儿时，就可以到水里游泳；他变成鸟儿时，就可以在天上飞行。
+ * <p>
+ * <p>
+ * 本例中，Component的角色便由鼎鼎大名的齐天大圣扮演；
+ * ConcreteComponent的角色属于大圣的本尊，就是猢狲本人；
+ * Decorator的角色由大圣的七十二变扮演。
+ * 而ConcreteDecorator的角色便是鱼儿、鸟儿等七十二般变化。
+ *
+ * @author andaicheng
+ * @version 2017/1/16
+ */
+package com.andyadc.designpattern.decorator.monkey;
---
+++ design-pattern/src/main/java/com/andyadc/designpattern/decorator/structure/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * 装饰模式以对客户透明的方式动态地给一个对象附加上更多的责任。换言之，
+ * 客户端并不会觉得对象在装饰前和装饰后有什么不同。
+ * 装饰模式可以在不使用创造更多子类的情况下，将对象的功能加以扩展。
+ *
+ * @author andaicheng
+ * @version 2017/1/16
+ */
+package com.andyadc.designpattern.decorator.structure;
