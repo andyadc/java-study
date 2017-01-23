2816171860e8c137c7b8d683e49c0313c420f902
andaicheng <andaicheng@gmail.com>
2017-01-23 12:24:37 +0800

    Command AudioPlayer commend interface

---
+++ design-pattern/src/main/java/com/andyadc/designpattern/command/audioplayer/Command.java
@@ -0,0 +1,15 @@
+package com.andyadc.designpattern.command.audioplayer;
+
+/**
+ * 抽象命令角色类
+ *
+ * @author andaicheng
+ * @version 2017/1/23
+ */
+public interface Command {
+
+    /**
+     * 执行方法
+     */
+    void execute();
+}
