fe6193807521e372268cffa0ce712f3e85244e0f
andaicheng <andaicheng@gmail.com>
2017-01-23 12:32:00 +0800

    Command AudioPlayer client(julia)

---
+++ design-pattern/src/main/java/com/andyadc/designpattern/command/audioplayer/Julia.java
@@ -0,0 +1,33 @@
+package com.andyadc.designpattern.command.audioplayer;
+
+/**
+ * 客户端角色，由茱丽小女孩扮演
+ *
+ * @author andaicheng
+ * @version 2017/1/23
+ */
+public class Julia {
+
+    public static void main(String[] args) {
+        //创建接收者对象
+        AudioPlayer player = new AudioPlayer();
+        //创建命令对象
+        Command playCommand = new PlayCommand(player);
+        Command rewindCommand = new RewindCommand(player);
+        Command stopCommand = new StopCommand(player);
+
+        //创建请求者对象
+        Keypad keypad = new Keypad();
+        keypad.setPlayCommand(playCommand);
+        keypad.setRewindCommand(rewindCommand);
+        keypad.setStopCommand(stopCommand);
+
+        //测试
+        keypad.play();
+        keypad.rewind();
+        keypad.stop();
+        keypad.play();
+        keypad.stop();
+    }
+
+}
