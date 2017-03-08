570340160e42c41669662bcc7dd22e6a87f2fd41
andaicheng <andaicheng@gmail.com>
2017-03-08 18:08:26 +0800

    chain of responsibility structure

--- design-pattern/src/main/java/com/andyadc/designpattern/chainofresponsibility/structure/Client.java
+++ design-pattern/src/main/java/com/andyadc/designpattern/chainofresponsibility/structure/Client.java
@@ -1,6 +1,9 @@
 package com.andyadc.designpattern.chainofresponsibility.structure;
 
 /**
+ * 客户端类
+ * 客户端创建了两个处理者对象，并指定第一个处理者对象的下家是第二个处理者对象，而第二个处理者对象没有下家。然后客户端将请求传递给第一个处理者对象。
+ *
  * @author andaicheng
  * @version 2017/3/8
  */
