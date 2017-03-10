16379cee1f50ca580913b44e05f5339e09809990
andaicheng <andaicheng@gmail.com>
2017-03-10 16:46:32 +0800

    CountDownLatch demo

---
+++ concurrency-study/src/main/java/com/andyadc/concurrency/latch/LatchDemo_1.java
@@ -0,0 +1,65 @@
+package com.andyadc.concurrency.latch;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.*;
+
+/**
+ * @author andaicheng
+ * @version 2017/3/10
+ */
+public class LatchDemo_1 {
+    public static void main(String[] args) throws Exception {
+        int num = 10;
+        //发令枪只只响一次
+        CountDownLatch begin = new CountDownLatch(1);
+        //参与跑步人数
+        CountDownLatch end = new CountDownLatch(num);
+
+        ExecutorService es = Executors.newFixedThreadPool(num);
+
+        //记录跑步成绩
+        List<Future<Integer>> futures = new ArrayList<>();
+        for (int i = 0; i < num; i++) {
+            futures.add(es.submit(new Runner(begin, end)));
+        }
+
+        begin.countDown();
+
+        end.await();
+
+        int sum = 0;
+        for (Future<Integer> f : futures) {
+            sum += f.get();
+        }
+
+        System.out.println("平均分数: " + (float) (sum / num));
+    }
+}
+
+class Runner implements Callable<Integer> {
+
+    //开始信号
+    private CountDownLatch begin;
+    //结束信号
+    private CountDownLatch end;
+
+    public Runner(CountDownLatch begin, CountDownLatch end) {
+        this.begin = begin;
+        this.end = end;
+    }
+
+    @Override
+    public Integer call() throws Exception {
+        //跑步成绩
+        int score = new Random().nextInt(10);
+        System.out.println("score:" + score);
+        //等待发令枪响
+        begin.await();
+        TimeUnit.SECONDS.sleep(score);
+        //跑步结束
+        end.countDown();
+        return score;
+    }
+}
