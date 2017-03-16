1e478bddaa169187e2529bbc8a937e9c571fc7a1
andyadc <andaicheng@gmail.com>
2017-03-16 21:44:34 +0800

    MyArrayList

---
+++ data-structure-and-algorithm/src/main/java/com/andyadc/datastructure/array/MyArrayList.java
@@ -0,0 +1,16 @@
+package com.andyadc.datastructure.array;
+
+import java.util.Iterator;
+
+/**
+ * @author andaicheng
+ * @version 2017/3/16
+ */
+public class MyArrayList<T> implements Iterable<T> {
+
+    @Override
+    public Iterator<T> iterator() {
+        return null;
+    }
+
+}
