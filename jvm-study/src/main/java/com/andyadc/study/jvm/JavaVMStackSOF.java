7e6616039e2b57d2c9329708f2988f48d381eefe
andaicheng <andaicheng@gmail.com>
2017-03-16 14:28:33 +0800

    jvm-study

---
+++ jvm-study/src/main/java/com/andyadc/study/jvm/JavaVMStackSOF.java
@@ -0,0 +1,25 @@
+package com.andyadc.study.jvm;
+
+/**
+ * @author andaicheng
+ * @version 2017/3/16
+ */
+public class JavaVMStackSOF {
+
+    private int stackLength = 1;
+
+    public void stackLeak() {
+        stackLength++;
+        stackLeak();
+    }
+
+    public static void main(String[] args) {
+        JavaVMStackSOF oom = new JavaVMStackSOF();
+        try {
+            oom.stackLeak();
+        } catch (Exception e) {
+            System.out.println("stack length: " + oom.stackLength);
+            throw e;
+        }
+    }
+}
