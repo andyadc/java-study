272221ace75dd95d4b746099503f1bf92f1f72db
andaicheng <andaicheng@gmail.com>
2017-03-21 18:33:44 +0800

    chainofresponsibility departmentbanquet DeptManager

---
+++ design-pattern/src/main/java/com/andyadc/designpattern/chainofresponsibility/departmentbanquet/DeptManager.java
@@ -0,0 +1,13 @@
+package com.andyadc.designpattern.chainofresponsibility.departmentbanquet;
+
+/**
+ * @author andaicheng
+ * @version 2017/3/21
+ */
+public class DeptManager extends Handler {
+
+    @Override
+    public String handleFeeRequest(String user, double fee) {
+        return null;
+    }
+}
