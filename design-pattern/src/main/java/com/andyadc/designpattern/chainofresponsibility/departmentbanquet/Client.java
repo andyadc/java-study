92c92fbb2ec21a58e8a46669959b307cc956c64a
andaicheng <andaicheng@gmail.com>
2017-03-21 18:43:57 +0800

    chainofresponsibility departmentbanquet Client

---
+++ design-pattern/src/main/java/com/andyadc/designpattern/chainofresponsibility/departmentbanquet/Client.java
@@ -0,0 +1,12 @@
+package com.andyadc.designpattern.chainofresponsibility.departmentbanquet;
+
+/**
+ * @author andaicheng
+ * @version 2017/3/21
+ */
+public class Client {
+
+    public static void main(String[] args) {
+
+    }
+}
