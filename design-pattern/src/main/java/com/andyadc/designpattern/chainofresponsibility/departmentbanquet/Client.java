b42ad4c879b77c1930a1a5617d8362065beceae1
andaicheng <andaicheng@gmail.com>
2017-03-21 18:54:26 +0800

    chainofresponsibility departmentbanquet Client

--- design-pattern/src/main/java/com/andyadc/designpattern/chainofresponsibility/departmentbanquet/Client.java
+++ design-pattern/src/main/java/com/andyadc/designpattern/chainofresponsibility/departmentbanquet/Client.java
@@ -7,6 +7,13 @@ package com.andyadc.designpattern.chainofresponsibility.departmentbanquet;
 public class Client {
 
     public static void main(String[] args) {
+        //先要组装责任链
+        Handler handler1 = new GeneralManager();
+        Handler handler2 = new DeptManager();
+        Handler handler3 = new ProjectManager();
+        handler2.setSuccessor(handler2);
+        handler1.setSuccessor(handler1);
 
+        String test1 = handler3.handleFeeRequest("张三", 300);
     }
 }
