38fde6af63bf2686aece7db51d9ec9a0ef55bbd4
andaicheng <andaicheng@gmail.com>
2017-03-22 15:29:49 +0800

    chainofresponsibility departmentbanquet Client

--- design-pattern/src/main/java/com/andyadc/designpattern/chainofresponsibility/departmentbanquet/Client.java
+++ design-pattern/src/main/java/com/andyadc/designpattern/chainofresponsibility/departmentbanquet/Client.java
@@ -11,9 +11,24 @@ public class Client {
         Handler handler1 = new GeneralManager();
         Handler handler2 = new DeptManager();
         Handler handler3 = new ProjectManager();
-        handler2.setSuccessor(handler2);
-        handler1.setSuccessor(handler1);
+        handler3.setSuccessor(handler2);
+        handler2.setSuccessor(handler1);
 
         String test1 = handler3.handleFeeRequest("张三", 300);
+        System.out.println("test1 = " + test1);
+        String test2 = handler3.handleFeeRequest("李四", 300);
+        System.out.println("test2 = " + test2);
+        System.out.println("---------------------------------------");
+
+        String test3 = handler3.handleFeeRequest("张三", 700);
+        System.out.println("test3 = " + test3);
+        String test4 = handler3.handleFeeRequest("李四", 700);
+        System.out.println("test4 = " + test4);
+        System.out.println("---------------------------------------");
+
+        String test5 = handler3.handleFeeRequest("张三", 1500);
+        System.out.println("test5 = " + test5);
+        String test6 = handler3.handleFeeRequest("李四", 1500);
+        System.out.println("test6 = " + test6);
     }
 }
