53392f4664a0f5c080caf5243785baeffed9ceb2
andyadc <andaicheng@gmail.com>
2017-03-26 15:15:35 +0800

    MyArrayList

--- data-structure-and-algorithm/src/main/java/com/andyadc/datastructure/array/MyArrayList.java
+++ data-structure-and-algorithm/src/main/java/com/andyadc/datastructure/array/MyArrayList.java
@@ -6,10 +6,40 @@ import java.util.Iterator;
  * @author andaicheng
  * @version 2017/3/16
  */
-public class MyArrayList<T> implements Iterable<T> {
+public class MyArrayList<AnyType> implements Iterable<AnyType> {
+
+    private static final int DEFAULT_CAPACITY = 10;
+
+    private int theSize;
+    private AnyType[] theItems;
+
+    public MyArrayList() {
+        clear();
+    }
+
+    public void clear() {
+        theSize = 0;
+        ensureCapacity(DEFAULT_CAPACITY);
+    }
+
+    public int size() {
+        return theSize;
+    }
+
+    public boolean isEmpty() {
+        return theSize == 0;
+    }
+
+    public void ensureCapacity(int newCapacity) {
+        if (newCapacity < theSize) {
+            return;
+        }
+        AnyType[] old = theItems;
+
+    }
 
     @Override
-    public Iterator<T> iterator() {
+    public Iterator<AnyType> iterator() {
         return null;
     }
 
