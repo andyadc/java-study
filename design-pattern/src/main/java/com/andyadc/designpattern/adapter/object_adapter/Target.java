4d685d38cdcf91cf1dc634adbfef5cca5d2697ff
andaicheng <andaicheng@gmail.com>
2017-03-28 17:48:37 +0800

    object adapter

---
+++ design-pattern/src/main/java/com/andyadc/designpattern/adapter/object_adapter/Target.java
@@ -0,0 +1,8 @@
+package com.andyadc.designpattern.adapter.object_adapter;
+
+/**
+ * @author andaicheng
+ * @version 2017/3/28
+ */
+public interface Target {
+}
---
+++ design-pattern/src/main/java/com/andyadc/designpattern/adapter/object_adapter/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * 对象适配器模式
+ *
+ * @author andaicheng
+ * @version 2017/3/28
+ */
+package com.andyadc.designpattern.adapter.object_adapter;
