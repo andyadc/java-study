febd54ab1786c95dbe97069e80901fd0a9637fc8
andyadc <andaicheng@gmail.com>
2017-03-31 20:53:00 +0800

    MyLinkedList

---
+++ data-structure-and-algorithm/src/main/java/com/andyadc/datastructure/array/MyLinkedList.java
@@ -0,0 +1,15 @@
+package com.andyadc.datastructure.array;
+
+import java.util.Iterator;
+
+/**
+ * @author andaicheng
+ * @version 2017/3/31
+ */
+public class MyLinkedList<AnyType> implements Iterable<AnyType> {
+
+    @Override
+    public Iterator<AnyType> iterator() {
+        return null;
+    }
+}
