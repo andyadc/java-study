267566106837085efaffdb72dd917b51b5292282
andyadc <andaicheng@gmail.com>
2017-04-04 12:43:11 +0800

    SortingAlgorithm

--- data-structure-and-algorithm/src/main/java/com/andyadc/algorithm/sorting/SortingAlgorithm.java
+++ data-structure-and-algorithm/src/main/java/com/andyadc/algorithm/sorting/SortingAlgorithm.java
@@ -4,6 +4,7 @@ package com.andyadc.algorithm.sorting;
  * @author andaicheng
  * @version 2017/4/4
  */
+@FunctionalInterface
 public interface SortingAlgorithm {
-    int doSort(int[] array);
+    int[] doSort(int[] array);
 }
