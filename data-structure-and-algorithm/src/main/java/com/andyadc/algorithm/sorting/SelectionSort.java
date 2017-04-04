8c36dc3dcc9ac04fa062e779799000fb16d512ed
andyadc <andaicheng@gmail.com>
2017-04-04 20:16:08 +0800

    SelectionSort

---
+++ data-structure-and-algorithm/src/main/java/com/andyadc/algorithm/sorting/SelectionSort.java
@@ -0,0 +1,13 @@
+package com.andyadc.algorithm.sorting;
+
+/**
+ * @author andaicheng
+ * @version 2017/4/4
+ */
+public class SelectionSort implements SortingAlgorithm {
+
+    @Override
+    public int[] doSort(int[] array) {
+        return new int[0];
+    }
+}
