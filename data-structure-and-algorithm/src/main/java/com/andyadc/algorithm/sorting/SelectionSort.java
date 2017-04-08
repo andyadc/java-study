ea459467e10e0993d08e5875e42cc367acd0af81
andyadc <andaicheng@gmail.com>
2017-04-08 23:26:13 +0800

    SelectionSort

--- data-structure-and-algorithm/src/main/java/com/andyadc/algorithm/sorting/SelectionSort.java
+++ data-structure-and-algorithm/src/main/java/com/andyadc/algorithm/sorting/SelectionSort.java
@@ -8,6 +8,16 @@ public class SelectionSort implements SortingAlgorithm {
 
     @Override
     public int[] doSort(int[] array) {
+        int min;
+        for (int i = 0; i < array.length - 1; i++) {
+            min = i;
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[j] < array[min]) {
+                    min = j;
+                }
+            }
+
+        }
         return new int[0];
     }
 }
