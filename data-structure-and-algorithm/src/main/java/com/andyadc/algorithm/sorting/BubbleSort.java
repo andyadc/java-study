509bfba29ef48a1564b68676cf899d7bc222c2e8
andyadc <andaicheng@gmail.com>
2017-04-09 15:00:13 +0800

    BubbleSort

--- data-structure-and-algorithm/src/main/java/com/andyadc/algorithm/sorting/BubbleSort.java
+++ data-structure-and-algorithm/src/main/java/com/andyadc/algorithm/sorting/BubbleSort.java
@@ -1,6 +1,11 @@
 package com.andyadc.algorithm.sorting;
 
 /**
+ * 冒泡排序
+ * 重复地遍历要排序的数组,比较每一对相邻的元素,如果它们次序错误,就把它们进行交换.
+ * 它的名字来自这种相对小的元素“上浮”到数组的顶部的方式.
+ * 因为它对元素只用到了比较的操作方式，所以是一种比较排序.
+ *
  * @author andaicheng
  * @version 2017/4/6
  */
@@ -8,6 +13,15 @@ public class BubbleSort implements SortingAlgorithm {
 
     @Override
     public int[] doSort(int[] array) {
-        return new int[0];
+        for (int i = 0; i < array.length - 1; i++) {
+            for (int j = 0; j < array.length - 1 - i; j++) {
+                if (array[j] > array[j + 1]) {
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+            }
+        }
+        return array;
     }
 }
--- data-structure-and-algorithm/src/test/java/com/andyadc/algorithm/sorting/test/SortingAlgorithmTest.java
+++ data-structure-and-algorithm/src/test/java/com/andyadc/algorithm/sorting/test/SortingAlgorithmTest.java
@@ -1,5 +1,6 @@
 package com.andyadc.algorithm.sorting.test;
 
+import com.andyadc.algorithm.sorting.BubbleSort;
 import com.andyadc.algorithm.sorting.SelectionSort;
 import com.andyadc.algorithm.sorting.SortingAlgorithm;
 import org.junit.Test;
@@ -15,7 +16,17 @@ public class SortingAlgorithmTest {
     @Test
     public void testSelectionSort() {
         SortingAlgorithm sorting = new SelectionSort();
-        int[] inputArray = {10, 34, 2, 56, 7, 67, 88, 42};
+        int[] inputArray = {10, 34, 2, 56, 7, 67, 0, 88, 42};
+        System.out.println("input: " + Arrays.toString(inputArray));
+
+        sorting.doSort(inputArray);
+        System.out.println("sorted output: " + Arrays.toString(inputArray));
+    }
+
+    @Test
+    public void testBubbleSort() {
+        SortingAlgorithm sorting = new BubbleSort();
+        int[] inputArray = {10, 34, 2, 56, 7, 67, 0, 88, 42};
         System.out.println("input: " + Arrays.toString(inputArray));
 
         sorting.doSort(inputArray);
