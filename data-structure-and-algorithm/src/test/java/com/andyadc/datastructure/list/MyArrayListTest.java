f9a1331762e0c4b2cb2cbf9c7cef388f565d1a57
andaicheng <andaicheng@gmail.com>
2017-04-19 15:24:20 +0800

    datastructure test

--- data-structure-and-algorithm/src/test/java/com/andyadc/datastructure/list/MyArrayListTest.java
+++ data-structure-and-algorithm/src/test/java/com/andyadc/datastructure/list/MyArrayListTest.java
@@ -1,8 +1,24 @@
 package com.andyadc.datastructure.list;
 
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * @author andaicheng
  * @version 2017/4/19
  */
 public class MyArrayListTest {
+
+    private static final Logger LOG = LoggerFactory.getLogger(MyArrayListTest.class);
+
+    @Test
+    public void testAdd() {
+        MyArrayList<Integer> list = new MyArrayList<>();
+        LOG.info("list size: {}", list.size());
+        list.add(1);
+        LOG.info("list size: {}", list.size());
+        LOG.info("list get: {}", list.get(0));
+    }
+
 }
--- data-structure-and-algorithm/src/test/java/com/andyadc/datastructure/list/MyLinkedListTest.java
+++ data-structure-and-algorithm/src/test/java/com/andyadc/datastructure/list/MyLinkedListTest.java
@@ -1,8 +1,20 @@
 package com.andyadc.datastructure.list;
 
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * @author andaicheng
  * @version 2017/4/19
  */
 public class MyLinkedListTest {
+
+    private static final Logger LOG = LoggerFactory.getLogger(MyLinkedListTest.class);
+
+    @Test
+    public void testAdd() {
+        MyLinkedList<String> list = new MyLinkedList<>();
+        LOG.info("list size: {}", list.size());
+    }
 }
