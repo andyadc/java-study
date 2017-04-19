a29f0398f80c8cffb34a987eea5a37aae5e81ce2
andaicheng <andaicheng@gmail.com>
2017-04-19 15:08:53 +0800

    datastructure test

---
+++ data-structure-and-algorithm/src/test/java/com/andyadc/datastructure/list/MyLinkedListTest.java
@@ -0,0 +1,8 @@
+package com.andyadc.datastructure.list;
+
+/**
+ * @author andaicheng
+ * @version 2017/4/19
+ */
+public class MyLinkedListTest {
+}
---
+++ data-structure-and-algorithm/src/test/java/com/andyadc/datastructure/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * @author andaicheng
+ * @version 2017/4/19
+ */
+package com.andyadc.datastructure;
