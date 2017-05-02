f15fe78a317e9b08f6f7c9f62f47ebd97e6ec886
andyadc <andaicheng@gmail.com>
2017-05-02 16:43:16 +0800

    Java8 functional interfaces

---
+++ java-basic/src/main/java/com/andyadc/study/java8/functional/package-info.java
@@ -0,0 +1,17 @@
+/**
+ * Java8 内置的四大核心函数式接口
+ * <p>
+ * <code>
+ * Consumer<T> : 消费型接口
+ * <p>
+ * Supplier<T> : 供给型接口
+ * <p>
+ * Fuction<T, R> : 函数型接口
+ * <p>
+ * Predicate<T> : 断言型接口
+ * </code>
+ *
+ * @author andaicheng
+ * @version 2017/5/1
+ */
+package com.andyadc.study.java8.functional;
