5cb275728039ad592f4fb32dbb3aa8fd256de240
andyadc <andaicheng@gmail.com>
2017-06-04 10:46:20 +0800

    TestGetClass

---
+++ java-basic/src/main/java/com/andyadc/study/reflection/TestGetClass.java
@@ -0,0 +1,25 @@
+package com.andyadc.study.reflection;
+
+/**
+ * @author andaicheng
+ * @version 2017/6/4
+ */
+public class TestGetClass {
+
+    public TestGetClass() {
+        System.out.println("constructor");
+    }
+
+    public static void main(String[] args) throws Exception {
+        Class clz = TestGetClass.class;
+        System.out.println(clz.newInstance());
+
+        // Class clz1 = new TestGetClass().getClass();
+        // System.out.println(clz1.newInstance());
+
+        // Class clz2 = Class.forName("com.andyadc.study.reflection.TestGetClass");
+        //System.out.println(clz2.newInstance());
+
+
+    }
+}
