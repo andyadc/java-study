97165ce63c12ff033c3bdd7124527c43acc61d99
andyadc <andaicheng@gmail.com>
2017-07-01 20:55:43 +0800

    RPC

---
+++ rpc-spring/src/main/java/com/andyadc/rpc/RpcEncoder.java
@@ -0,0 +1,27 @@
+package com.andyadc.rpc;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.handler.codec.MessageToByteEncoder;
+
+/**
+ * @author andaicheng
+ * @version 2017/7/1
+ */
+public class RpcEncoder extends MessageToByteEncoder {
+
+    private Class<?> genericClass;
+
+    public RpcEncoder(Class<?> genericClass) {
+        this.genericClass = genericClass;
+    }
+
+    @Override
+    protected void encode(ChannelHandlerContext ctx, Object msg, ByteBuf out) throws Exception {
+        if (genericClass.isInstance(msg)) {
+            byte[] data = SerializationUtils.serialize(msg);
+            out.writeInt(data.length);
+            out.writeBytes(data);
+        }
+    }
+}
