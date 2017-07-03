1e39f555766feab6c0b44c3defad8c7228cca71a
andaicheng <andaicheng@gmail.com>
2017-07-03 18:48:40 +0800

    rpc sample

---
+++ rpc-spring-sample-server/src/main/java/com/andyadc/rpc/sample/server/RpcBootstrap.java
@@ -0,0 +1,12 @@
+package com.andyadc.rpc.sample.server;
+
+/**
+ * @author andaicheng
+ * @version 2017/7/3
+ */
+public class RpcBootstrap {
+
+    public static void main(String[] args) {
+
+    }
+}
