6d0b226d5c165e8419046a8af3b53d3726ea2629
andaicheng <andaicheng@gmail.com>
2017-07-12 10:40:07 +0800

    rpc

---
+++ rpc-spring/src/main/java/com/andyadc/rpc/RpcServer.java
@@ -0,0 +1,91 @@
+package com.andyadc.rpc;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * @author andaicheng
+ * @version 2017/7/12
+ */
+public class RpcServer implements ApplicationContextAware, InitializingBean {
+
+    private static final Logger LOG = LoggerFactory.getLogger(RpcServer.class);
+
+    private Map<String, Object> handlerMap = new ConcurrentHashMap<>(); // 存放接口名与服务对象之间的映射关系
+
+    private String serverAddress;
+    private ServiceRegistry serviceRegistry;
+
+    public RpcServer(String serverAddress) {
+        this.serverAddress = serverAddress;
+    }
+
+    public RpcServer(String serverAddress, ServiceRegistry serviceRegistry) {
+        this.serverAddress = serverAddress;
+        this.serviceRegistry = serviceRegistry;
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        Map<String, Object> rpcServiceMap = applicationContext.getBeansWithAnnotation(RpcService.class);
+        if (rpcServiceMap != null && !rpcServiceMap.isEmpty()) {
+            for (Object serviceBean : rpcServiceMap.values()) {
+                String interfaceName = serviceBean.getClass().getAnnotation(RpcService.class).value().getName();
+                handlerMap.put(interfaceName, serviceBean);
+            }
+        }
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        EventLoopGroup bossGroup = new NioEventLoopGroup();
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+
+        try {
+            ServerBootstrap bootstrap = new ServerBootstrap();
+            bootstrap.group(bossGroup, workerGroup).channel(NioServerSocketChannel.class)
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        public void initChannel(SocketChannel channel) throws Exception {
+                            channel.pipeline()
+                                    .addLast(new RpcDecoder(RpcRequest.class)) // 将 RPC 请求进行解码（为了处理请求）
+                                    .addLast(new RpcEncoder(RpcResponse.class)) // 将 RPC 响应进行编码（为了返回响应）
+                                    .addLast(new RpcHandler(handlerMap)); // 处理 RPC 请求
+                        }
+                    })
+                    .option(ChannelOption.SO_BACKLOG, 128)
+                    .childOption(ChannelOption.SO_KEEPALIVE, true);
+
+            String[] array = serverAddress.split(":");
+            String host = array[0];
+            int port = Integer.parseInt(array[1]);
+
+            ChannelFuture future = bootstrap.bind(host, port).sync();
+            LOG.debug("server started on port {}", port);
+
+            if (serviceRegistry != null) {
+                serviceRegistry.register(serverAddress); // 注册服务地址
+            }
+
+            future.channel().closeFuture().sync();
+        } finally {
+            workerGroup.shutdownGracefully();
+            bossGroup.shutdownGracefully();
+        }
+    }
+}
