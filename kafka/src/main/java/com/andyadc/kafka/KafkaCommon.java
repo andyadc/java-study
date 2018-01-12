2fd1dfa02549c938220b145d813d0c79c3c02396
99BILL\andy.an <andy.an@99bill.com>
2018-01-12 17:17:00 +0800

    Kafka

---
+++ kafka/src/main/java/com/andyadc/kafka/KafkaCommon.java
@@ -0,0 +1,21 @@
+package com.andyadc.kafka;
+
+/**
+ * @author andy.an
+ * @since 2018/1/12
+ */
+public class KafkaCommon {
+
+    /**
+     * 连接 Zk
+     */
+    protected static final String ZK_CONNECT = "http://www.jd-server.com:2181";
+    /**
+     * session 过期时间
+     */
+    protected static final int SESSION_TIMEOUT = 30000;
+    /**
+     * 连接超时时间
+     */
+    protected static final int CONNECT_TIMEOUT = 30000;
+}
